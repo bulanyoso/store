57c1dc5ecde5a8f99a77b8db9e36a1dcd50c8899
bulanyoso <cchcc3@gmail.com>
2021-02-04 04:29:34 +0900

    v1

---
+++ src/main/java/cardealer/StoreManageRepository.java
@@ -0,0 +1,8 @@
+package cardealer;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface StoreManageRepository extends PagingAndSortingRepository<StoreManage, Long>{
+
+
+}
